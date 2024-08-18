e8eee7ec4897f7179ecc322fb42ea71faa7b0a68
Savva Voloshin <savva.voloshin@gmail.com>
2024-08-18 19:24:15 +0300

    *content

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Гравёр Этевентин',
+    template: '%s | Гравёр Этевентин',
   },
   description: 'This is my portfolio.',
   openGraph: {
