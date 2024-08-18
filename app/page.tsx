4b5606c967e0c653ff0df0967fb27a339bc16fc0
Savva Voloshin <savva.voloshin@gmail.com>
2024-08-18 17:34:33 +0300

    *content

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Гравёр, охотник и косторез: Этевентин Станислав Викторович
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Уэлен - край охотников, косторезов - не тронутый цивилизацией. Хотя, чего уж - тронутый. Да и не все - охотники.`}
       </p>
       <div className="my-8">
         <BlogPosts />
